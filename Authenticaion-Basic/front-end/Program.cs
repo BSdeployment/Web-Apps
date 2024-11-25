5874a2edfdfe7dd393c54e6b03815294e160ae67
BSdeployment <142491330+BSdeployment@users.noreply.github.com>
2024-11-25 10:31:07 +0200

    Add files via upload

---
+++ Authenticaion-Basic/front-end/Program.cs
@@ -0,0 +1,14 @@
+using BlazorAuthUsresBasic;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddFluentUIComponents();
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+
+await builder.Build().RunAsync();
